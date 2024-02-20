f8c974c42e22aa8011201f0bad0d02753a740d3f
S-ChandanaSri <chandanaofficial8@gmail.com>
2024-02-20 15:49:25 +0530

    added changes

--- server.js
+++ server.js
@@ -92,10 +92,10 @@ app.use(bodyParser.json());
 
 
 const db = mysql.createConnection({
-    host: 'users.cfmaq2s00dwp.ap-southeast-2.rds.amazonaws.com',
-    user: 'admin',
-    database: 'users',
-    password:'chanduchandu'
+    host: 'localhost',
+    user: 'root',
+    database: 'login',
+    password:'123456'
   });
 
 
